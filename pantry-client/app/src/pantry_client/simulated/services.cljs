fb6609b53a9e78bbef7ae0879110bbcd34d251e7
David Young-Chan Kay <davidykay@gmail.com>
2013-03-31 12:18:43 -0400

    initial import of project skeleton

---
+++ pantry-client/app/src/pantry_client/simulated/services.cljs
@@ -0,0 +1,3 @@
+(ns pantry-client.simulated.services)
+
+;; Implement services to simulate talking to back-end services
---
+++ pantry-client/project.clj
@@ -0,0 +1,13 @@
+(defproject pantry-client "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/clojurescript "0.0-1586"]
+                 [domina "1.0.1"]
+                 [ch.qos.logback/logback-classic "1.0.6"]
+                 [io.pedestal/pedestal.app "0.1.2-SNAPSHOT"]
+                 [io.pedestal/pedestal.app-tools "0.1.2-SNAPSHOT"]]
+  :profiles {:dev {:source-paths ["dev"]}}
+  :min-lein-version "2.0.0"
+  :source-paths ["app/src" "app/templates"]
+  :resource-paths ["config"]
+  :aliases {"dumbrepl" ["trampoline" "run" "-m" "clojure.main/main"]})
